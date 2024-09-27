ff7a90312aaf4f5c68b5cedf05410f6e41db20fe
GisellySouza <gidesouza1234@gmail.com>
2024-09-27 14:35:24 -0300

    feat/ create-theme

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    body, html {
+        margin: 0;
+        padding: 0;
+        font-family: 'Inter', sans-serif;
+    }
+
+    h1, h2, h3, h4, h5, h6, p, ul, il {
+        margin: 0;
+        padding: 0;
+    }
+`
